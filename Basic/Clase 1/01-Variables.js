2a32e349b2269850f75a1cb228c9210b9e222e9f
sangaray <scgaraycochea@yahoo.com.ar>
2024-08-30 17:26:05 -0300

    Curso JS - Variables

---
+++ Basic/Clase 1/01-Variables.js
@@ -0,0 +1,33 @@
+// Variables
+// Guardar valores para volver a utilizarlos
+// JS se va ejecutando de forma secuencial.
+// Los nombres de las variables se escriben en lowerCamelCase
+// Formas de crear variables
+// Var, esta es la manera en que NO se debe trabajar hoy, era la forma en que se creaban inicialmente. Esta opción puede crear muchos problemas
+var helloWorld = "Hola, JavaScript";
+
+console.log(helloWorld);
+
+helloWorld = "Hola de nuevo, JavaScript";
+
+console.log(helloWorld);
+
+// Let
+let helloWorld2 = "Hola, JavaScript";
+
+console.log(helloWorld2);
+
+helloWorld2 = "Hola de nuevo, JavaScript";
+
+console.log(helloWorld2);
+
+// const
+const helloWorld3 = "Hola, JavaScript";
+
+console.log(helloWorld3);
+
+/* helloWorld3 = "Hola de nuevo, JavaScript";
+
+console.log(helloWorld3); */
+
+// let cuando varia el dato y con const cuando no varía, estas son las formas de trabajar variables

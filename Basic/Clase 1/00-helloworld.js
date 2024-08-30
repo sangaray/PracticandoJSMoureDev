fb9b7fbb14300d720152f1629d99a2c16aee32c5
sangaray <scgaraycochea@yahoo.com.ar>
2024-08-30 16:43:09 -0300

    Curso JS - Comentarios y Números

--- Basic/Clase 1/00-helloworld.js
+++ Basic/Clase 1/00-helloworld.js
@@ -1,3 +1,24 @@
+// COMENTARIO
+
+// Esto es un comentario simple
+/* Esto es 
+  un comentario 
+  en varias líneas
+*/
+
+// Se usa para dejar notas o para evitar que se ejecute alguna línea de código
+
 console.log("Hola, JavaScript");
 console.log("Hola, JavaScript");
 console.log(`Hola, JavaScript`);
+
+// console.log(`Hola, JavaScript`);
+
+// Trabajando Números
+console.log(5);
+console.log("5");
+console.log(5 - 2);
+console.log(5 + 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
